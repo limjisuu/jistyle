3ccfe35917b0cf52cc9931d905c40662cb897be7
limjisuu <dlawltn1999@naver.com>
2020-05-30 08:12:51 +0900

    Updates

---
+++ precache-manifest.3cc95d7f86e327a96c1e63fd5e10413d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b841da260a546677204cf12be6cdf1",
+    "url": "/jistyle/index.html"
+  },
+  {
+    "revision": "202a8afae5b7c9195702",
+    "url": "/jistyle/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3794ccbc853774e9192d",
+    "url": "/jistyle/static/js/2.666db0c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jistyle/static/js/2.666db0c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202a8afae5b7c9195702",
+    "url": "/jistyle/static/js/main.bdcbf6a3.chunk.js"
+  },
+  {
+    "revision": "5aa86e32d4f1e3041a8c",
+    "url": "/jistyle/static/js/runtime-main.a5c61e06.js"
+  }
+]);
